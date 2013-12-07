2a5c51629cb3209ff1de6827b83f608c6a1473f8
Aaron Munguia <aamm89@gmail.com>
2013-12-07 13:41:40 -0600

    add static functions camelize and humanize

--- flavor/classes/Inflector.class.php
+++ flavor/classes/Inflector.class.php
@@ -9,71 +9,71 @@
   */
 class Inflector {
 
-	public static function pluralize($word)
+    public static function pluralize($word)
     {
-		$word = strtolower($word);
+        $word = strtolower($word);
         $plural = array('/(s)tatus$/i' => '\1\2tatuses',
-									'/(quiz)$/i' => '\1zes',
-									'/^(ox)$/i' => '\1\2en',
-									'/([m|l])ouse$/i' => '\1ice',
-									'/(matr|vert|ind)(ix|ex)$/i'  => '\1ices',
-									'/(x|ch|ss|sh)$/i' => '\1es',
-									'/([^aeiouy]|qu)y$/i' => '\1ies',
-									'/(hive)$/i' => '\1s',
-									'/(?:([^f])fe|([lr])f)$/i' => '\1\2ves',
-									'/sis$/i' => 'ses',
-									'/([ti])um$/i' => '\1a',
-									'/(p)erson$/i' => '\1eople',
-									'/(m)an$/i' => '\1en',
-									'/(c)hild$/i' => '\1hildren',
-									'/(buffal|tomat)o$/i' => '\1\2oes',
-									'/(alumn|bacill|cact|foc|fung|nucle|radi|stimul|syllab|termin|vir)us$/i' => '\1i',
-									'/us$/' => 'uses',
-									'/(alias)$/i' => '\1es',
-									'/(ax|cri|test)is$/i' => '\1es',
-									'/s$/' => 's',
-									'/$/' => 's',);
+                                    '/(quiz)$/i' => '\1zes',
+                                    '/^(ox)$/i' => '\1\2en',
+                                    '/([m|l])ouse$/i' => '\1ice',
+                                    '/(matr|vert|ind)(ix|ex)$/i'  => '\1ices',
+                                    '/(x|ch|ss|sh)$/i' => '\1es',
+                                    '/([^aeiouy]|qu)y$/i' => '\1ies',
+                                    '/(hive)$/i' => '\1s',
+                                    '/(?:([^f])fe|([lr])f)$/i' => '\1\2ves',
+                                    '/sis$/i' => 'ses',
+                                    '/([ti])um$/i' => '\1a',
+                                    '/(p)erson$/i' => '\1eople',
+                                    '/(m)an$/i' => '\1en',
+                                    '/(c)hild$/i' => '\1hildren',
+                                    '/(buffal|tomat)o$/i' => '\1\2oes',
+                                    '/(alumn|bacill|cact|foc|fung|nucle|radi|stimul|syllab|termin|vir)us$/i' => '\1i',
+                                    '/us$/' => 'uses',
+                                    '/(alias)$/i' => '\1es',
+                                    '/(ax|cri|test)is$/i' => '\1es',
+                                    '/s$/' => 's',
+                                    '/$/' => 's',);
         
         $uncountable = array('.*[nrlm]ese', '.*deer', '.*fish', '.*measles', '.*ois', '.*pox', '.*sheep', 'Amoyese',
-											'bison', 'Borghese', 'bream', 'breeches', 'britches', 'buffalo', 'cantus', 'carp', 'chassis', 'clippers',
-											'cod', 'coitus', 'Congoese', 'contretemps', 'corps', 'debris', 'diabetes', 'djinn', 'eland', 'elk',
-											'equipment', 'Faroese', 'flounder', 'Foochowese', 'gallows', 'Genevese', 'Genoese', 'Gilbertese', 'graffiti',
-											'headquarters', 'herpes', 'hijinks', 'Hottentotese', 'information', 'innings', 'jackanapes', 'Kiplingese',
-											'Kongoese', 'Lucchese', 'mackerel', 'Maltese', 'media', 'mews', 'moose', 'mumps', 'Nankingese', 'news',
-											'nexus', 'Niasese', 'Pekingese', 'Piedmontese', 'pincers', 'Pistoiese', 'pliers', 'Portuguese', 'proceedings',
-											'rabies', 'rice', 'rhinoceros', 'salmon', 'Sarawakese', 'scissors', 'sea[- ]bass', 'series', 'Shavese', 'shears',
-											'siemens', 'species', 'swine', 'testes', 'trousers', 'trout', 'tuna', 'Vermontese', 'Wenchowese',
-											'whiting', 'wildebeest', 'Yengeese',);
+                                            'bison', 'Borghese', 'bream', 'breeches', 'britches', 'buffalo', 'cantus', 'carp', 'chassis', 'clippers',
+                                            'cod', 'coitus', 'Congoese', 'contretemps', 'corps', 'debris', 'diabetes', 'djinn', 'eland', 'elk',
+                                            'equipment', 'Faroese', 'flounder', 'Foochowese', 'gallows', 'Genevese', 'Genoese', 'Gilbertese', 'graffiti',
+                                            'headquarters', 'herpes', 'hijinks', 'Hottentotese', 'information', 'innings', 'jackanapes', 'Kiplingese',
+                                            'Kongoese', 'Lucchese', 'mackerel', 'Maltese', 'media', 'mews', 'moose', 'mumps', 'Nankingese', 'news',
+                                            'nexus', 'Niasese', 'Pekingese', 'Piedmontese', 'pincers', 'Pistoiese', 'pliers', 'Portuguese', 'proceedings',
+                                            'rabies', 'rice', 'rhinoceros', 'salmon', 'Sarawakese', 'scissors', 'sea[- ]bass', 'series', 'Shavese', 'shears',
+                                            'siemens', 'species', 'swine', 'testes', 'trousers', 'trout', 'tuna', 'Vermontese', 'Wenchowese',
+                                            'whiting', 'wildebeest', 'Yengeese',);
 
         $irregular = array('atlas' => 'atlases',
-										'beef' => 'beefs',
-										'brother' => 'brothers',
-										'child' => 'children',
-										'corpus' => 'corpuses',
-										'cow' => 'cows',
-										'ganglion' => 'ganglions',
-										'genie' => 'genies',
-										'genus' => 'genera',
-										'graffito' => 'graffiti',
-										'hoof' => 'hoofs',
-										'loaf' => 'loaves',
-										'man' => 'men',
-										'money' => 'monies',
-										'mongoose' => 'mongooses',
-										'move' => 'moves',
-										'mythos' => 'mythoi',
-										'numen' => 'numina',
-										'occiput' => 'occiputs',
-										'octopus' => 'octopuses',
-										'opus' => 'opuses',
-										'ox' => 'oxen',
-										'penis' => 'penises',
-										'person' => 'people',
-										'sex' => 'sexes',
-										'soliloquy' => 'soliloquies',
-										'testis' => 'testes',
-										'trilby' => 'trilbys',
-										'turf' => 'turfs',);
+                                        'beef' => 'beefs',
+                                        'brother' => 'brothers',
+                                        'child' => 'children',
+                                        'corpus' => 'corpuses',
+                                        'cow' => 'cows',
+                                        'ganglion' => 'ganglions',
+                                        'genie' => 'genies',
+                                        'genus' => 'genera',
+                                        'graffito' => 'graffiti',
+                                        'hoof' => 'hoofs',
+                                        'loaf' => 'loaves',
+                                        'man' => 'men',
+                                        'money' => 'monies',
+                                        'mongoose' => 'mongooses',
+                                        'move' => 'moves',
+                                        'mythos' => 'mythoi',
+                                        'numen' => 'numina',
+                                        'occiput' => 'occiputs',
+                                        'octopus' => 'octopuses',
+                                        'opus' => 'opuses',
+                                        'ox' => 'oxen',
+                                        'penis' => 'penises',
+                                        'person' => 'people',
+                                        'sex' => 'sexes',
+                                        'soliloquy' => 'soliloquies',
+                                        'testis' => 'testes',
+                                        'trilby' => 'trilbys',
+                                        'turf' => 'turfs',);
 
         $lowercased_word = strtolower($word);
 
@@ -96,5 +96,26 @@ class Inflector {
         }
         return false;
 
+    }
+
+    /**
+     * Returns the given lower_case_and_underscored_word as a CamelCased word. 
+     *
+     * @param string $lowerCaseAndUnderscoredWord Word to camelize
+     * @return string Camelized word. LikeThis.
+     */
+    public static function camelize($lowerCaseAndUnderscoredWord){
+        return str_replace(' ', '', Inflector::humanize($lowerCaseAndUnderscoredWord));
+    }
+
+    /**
+     * Returns the given underscored_word_group as a Human Readable Word Group.
+     * (Underscores are replaced by spaces and capitalized following words.)
+     *
+     * @param string $lowerCaseAndUnderscoredWord String to be made more readable
+     * @return string Human-readable string
+     */
+    public static function humanize($lowerCaseAndUnderscoredWord) {
+        return ucwords(str_replace('_', ' ', $lowerCaseAndUnderscoredWord));
     }
 }
