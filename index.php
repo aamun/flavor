56e5cc57ac89ff4628a955cf74152950617ab19d
Mikeroguez <mike.roguez@gmail.com>
2014-03-26 18:51:32 -0600

    1.2 con flavor_autoload()

--- index.php
+++ index.php
@@ -31,7 +31,7 @@ if (!file_exists($configFile)) {
 	}
 }
 
-function __autoload($className) {
+function flavor_autoload($className) {
 	$directories = array(
 		Absolute2Flavor.'flavor'.DIRSEP.'classes'.DIRSEP.$className.'.class.php', // Flavor classes
 		Absolute2Flavor.'flavor'.DIRSEP.'interfaces'.DIRSEP.$className.'.interface.php', // maybe we want an interface
@@ -57,8 +57,15 @@ function __autoload($className) {
 	}
 }
 
-//We register our flavor autoload
-spl_autoload_register('__autoload');
+if(function_exists('spl_autoload_register')) {
+	//We register our flavor autoload (flavor_autoload function)
+	spl_autoload_register('flavor_autoload');
+} else {
+	//We hope nobody else uses __autoload :/
+	function __autoload($className) {
+		flavor_autoload($className);
+	}
+}
 
 // 'Globals' to be used throughout the application
 // usign the _Registry Pattern_
